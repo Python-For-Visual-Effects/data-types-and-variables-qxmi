585de9a82b15e33f278e8a09f3d2a60a476dbd9f
qxmi <qxie15@myseneca.ca>
2020-10-03 13:44:44 -0400

    answer five

--- main.py
+++ main.py
@@ -29,3 +29,6 @@ print(len(sentence))
 # 5.- Given the resolution 1920 x 1080, make a program that prints a string with 
 # the 10% over-scan value of those numbers. The printed string must be as 
 # follows: "The 10% overscan of 1920 is <value 1>, and the 1080 is <value 2>"
+value_one = 1920*1.1
+value_two = 1080*1.1
+print("the 10% over-scan 1920 is " + str(value_one) + ", and the 1080 is " + str(value_two))
